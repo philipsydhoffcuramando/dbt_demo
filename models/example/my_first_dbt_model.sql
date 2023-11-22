295cc4655210ee6cf3cff4eef1ad71d594cfee89
Philip Sydhoff <philip.sydhoff@curamando.com>
2023-11-22 21:24:50 +0000

    commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,16 +9,15 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
+with test_table as (
 select *
-from source_data
+from `curamando-snowplow.rt_pipeline_prod1.events`
+where
+    timestamp_trunc(collector_tstamp, day)
+    between timestamp("2023s-09-21") and timestamp("2023-11-21")
+limit 1000
+)
+select * from test_table
 
 /*
     Uncomment the line below to remove records with null `id` values
